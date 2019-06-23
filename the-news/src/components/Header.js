ee7f784cd62fcb662ac0845aa14a41be6972bc9e
andrew <burns.andrew@hotmail.co.uk>
2019-06-23 18:43:26 +0100

    links established between components; no need for maual url entry now

--- the-news/src/components/Header.js
+++ the-news/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const Header = props => {
   return (
@@ -6,6 +7,36 @@ const Header = props => {
       <div className="nav">
         <h1>THE News</h1>
       </div>
+      <nav>
+        <ul className="mainMenu">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/articles">
+            <li>Articles</li>
+          </Link>
+          <Link to="/topics">
+            <li>Topics</li>
+          </Link>
+          {!props.loggedInUser ? (
+            <Link to="/login">
+              <li>Log In</li>
+            </Link>
+          ) : (
+            <div className="loggedIn">
+              <Link to="/user">
+                <li>My Profile</li>
+              </Link>
+              <li>
+                Hey {props.loggedInUser}, good to see you again{" "}
+                <button className="logoutButton" onClick={props.logOutUser}>
+                  Log out
+                </button>
+              </li>
+            </div>
+          )}
+        </ul>
+      </nav>
     </header>
   );
 };
