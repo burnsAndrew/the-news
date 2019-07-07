4334f5a8c48cd7caf3f920bf9c8abb79aa1d38ea
andrew <burns.andrew@hotmail.co.uk>
2019-07-07 15:25:55 +0100

    css styling for headers and home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ const Home = () => {
   return (
     <>
       <div className="home" id="backgroundImage">
-        <h2 id="subHeader">THE facts.</h2>
-        <h2 id="subHeader">THE stories.</h2>
-        <h2 id="subHeader">THE news.</h2>
+        <h2 id="subHeaderHome">THE facts.</h2>
+        <h2 id="subHeaderHome">THE stories.</h2>
+        <h2 id="subHeaderHome">THE news.</h2>
       </div>
     </>
   );
